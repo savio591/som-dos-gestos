a4302bf79c263cd26f25aee99d93f9534dcebda9
savio591 <savio591@hotmail.com>
2021-01-08 02:39:28 +0000

    Deploy de teste para https://savilexperiments.com.br/som-dos-gestos-HackathonSonyCodeStage

---
+++ docs/precache-manifest.2e51e499aefa67d74385c6c5e8fd4eee.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6da0b5c076a4ebb8330b5d77302d1391",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20533febb1ec4ca8f876",
+    "url": "/static/css/main.c46d0746.chunk.css"
+  },
+  {
+    "revision": "b49b086232abd021a7de",
+    "url": "/static/js/2.7a4c3d15.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.7a4c3d15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20533febb1ec4ca8f876",
+    "url": "/static/js/main.f76f3613.chunk.js"
+  },
+  {
+    "revision": "14114f6bbf13cf3266e3",
+    "url": "/static/js/runtime-main.7fd8ea6a.js"
+  },
+  {
+    "revision": "e35f0e48c3b5d6287f9e4dc665cbd0e2",
+    "url": "/static/media/back.e35f0e48.svg"
+  },
+  {
+    "revision": "1f05d0996088a47e0764ff4dd3c5e185",
+    "url": "/static/media/play.1f05d099.svg"
+  },
+  {
+    "revision": "235640218bd537351409ed99137fce69",
+    "url": "/static/media/search.23564021.svg"
+  },
+  {
+    "revision": "51131314ba5e2206e4b1714302c7ebab",
+    "url": "/static/media/settings.51131314.svg"
+  }
+]);
